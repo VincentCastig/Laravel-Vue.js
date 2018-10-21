b980dd061ceb277b5c5410cc96d3f862bf9efae0
VincentCastig <vincent.castig@gmail.com>
2018-10-21 08:21:20 -0500

    Rapid Authentication

--- Blog/resources/views/layouts/nav.blade.php
+++ Blog/resources/views/layouts/nav.blade.php
@@ -6,6 +6,33 @@
         <a class="blog-nav-item" href="#">Press</a>
         <a class="blog-nav-item" href="#">New hires</a>
         <a class="blog-nav-item" href="#">About</a>
+
+        @guest
+          <a class="blog-nav-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+            @if (Route::has('register'))
+              <a class="blog-nav-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+            @endif
+                      
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="blog-nav-item nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+      
       </nav>
     </div>
   </div>
